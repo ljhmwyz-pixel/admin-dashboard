6ad3f1f6cb5b1674ef77117a2e316bc48b8d95fd
Zhang rui <1694246305@qq.com>
2026-02-27 19:25:17 +0800

    布局

--- src/components/layouts/SiderFooter.tsx
+++ src/components/layouts/SiderFooter.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Dropdown, Menu } from 'antd'
 import type { MenuProps } from 'antd';
-import {
-  LogoutOutlined,
-  BgColorsOutlined,
-  GlobalOutlined
-} from '@ant-design/icons';
 import avatarIcon from '@/assets/images/layout/avatar_icon.png'
 import moreIcon from '@/assets/images/layout/more_icon.png'
 import styles from './SiderFooter.module.scss'
@@ -16,8 +11,23 @@ interface Props {
 }
 
 const SiderFooter: React.FC<Props> = ({ collapsed }) => {
-
+  const [expanded, setExpanded] = useState(false)
   const userMenuItems: MenuProps['items'] = [
+    {
+      key: '6',
+      label: '6666',
+    },
+    {
+      key: '7',
+      label: '777',
+    },
+    {
+      key: '8',
+      label: '8888',
+    },
+  ]
+
+  const languageItems: MenuProps['items'] = [
     {
       key: '1',
       label: '简体中文',
@@ -42,24 +52,16 @@ const SiderFooter: React.FC<Props> = ({ collapsed }) => {
 
   return (
     <div
-      className={`${styles.sideMenuContainer} ${collapsed ? styles.sideMenuContainerCollapsed : ''}`}
+      className={`${styles.sideMenuContainer} ${collapsed ? styles.sideMenuContainerCollapsed : ''} ${expanded ? styles.expanded : ''}`}
+      onMouseEnter={() => setExpanded(true)}
+      onMouseLeave={() => setExpanded(false)}
     >
       {/* 展开后的内容 */}
       {!collapsed && (
         <>
           {/* 扩展内容 */}
           <div className={styles.expandArea}>
-            <div className={styles.extraItem}>
-              <div className={styles.leftInfo}>
-                <div className={`${styles.leftIcon} ${styles.leftIcon1}`} />
-                <span className={`${styles.extraItemText} ${styles.extraItemText1}`}>Pylontech</span>
-              </div>
-              <div className={styles.arrowContainer}>
-                <div className={styles.rightArrowIcon} />
-              </div>
-            </div>
             <Dropdown
-              // menu={{ items:  }}
               trigger={['hover']}
               placement="bottomRight"
               align={{
@@ -73,6 +75,33 @@ const SiderFooter: React.FC<Props> = ({ collapsed }) => {
                   })}
                 </div>
               )}
+              onOpenChange={(open) => setExpanded(open)}
+            >
+              <div className={styles.extraItem}>
+                <div className={styles.leftInfo}>
+                  <div className={`${styles.leftIcon} ${styles.leftIcon1}`} />
+                  <span className={`${styles.extraItemText} ${styles.extraItemText1}`}>Pylontech</span>
+                </div>
+                <div className={styles.arrowContainer}>
+                  <div className={styles.rightArrowIcon} />
+                </div>
+              </div>
+            </Dropdown>
+            <Dropdown
+              trigger={['hover']}
+              placement="bottomRight"
+              align={{
+                points: ['bl', 'tl'],
+                offset: [186, 74],
+              }}
+              popupRender={() => (
+                <div className={styles.dropdownStyle}>
+                  {languageItems.map((item: any) => {
+                    return <div key={item?.key} className={styles.dropdownItem}>{item.label}</div>
+                  })}
+                </div>
+              )}
+              onOpenChange={(open) => setExpanded(open)}
             >
               <div className={styles.extraItem}>
                 <div className={styles.leftInfo}>
@@ -104,8 +133,6 @@ const SiderFooter: React.FC<Props> = ({ collapsed }) => {
       {collapsed && <div className={styles.userContainerCollapsed}>
         <img src={avatarIcon} className={styles.avatar} alt='avatar' />
       </div>}
-      {/* 第二层级弹出框 */}
-      {/* <div className={styles.dropdownContainer}>12312</div> */}
     </div>
   )
 }
