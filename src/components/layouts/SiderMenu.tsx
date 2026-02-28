ec8184c5519a48da9d859ca5e14a868ebd3c32e2
Zhang rui <1694246305@qq.com>
2026-02-28 10:28:10 +0800

    修改

--- src/components/layouts/SiderMenu.tsx
+++ src/components/layouts/SiderMenu.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Menu, ConfigProvider, type MenuProps } from 'antd'
-import { useNavigate } from 'react-router-dom';
+import { Menu, ConfigProvider, Popover } from 'antd'
+import type { MenuProps, PopoverProps } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
 import orgActiveIcon from '@/assets/images/layout/org_active_icon.png'
 import orgDefaultIcon from '@/assets/images/layout/org_default_icon.png'
@@ -17,20 +18,41 @@ interface Props {
 }
 
 const SiderFooter: React.FC<Props> = ({ collapsed }) => {
-  const navigate = useNavigate();
-  const [selectedKeys, setSelectedKeys] = useState(['org-list']);
-  const menuItems = [
+  const navigate = useNavigate()
+  const [selectedKeys, setSelectedKeys] = useState<string[]>(['orglist'])
+
+  const handleMenuClick = (key: string) => {
+    setSelectedKeys([key])
+
+    if (key === 'orglist') navigate('/organization/list')
+    if (key === 'orgtype') navigate('/organization/type')
+    if (key === 'role') navigate('/role')
+    if (key === 'user') navigate('/user')
+  }
+
+  const menuItems: MenuProps['items'] = [
     {
       label: <span className={styles.menuItemLabelStyle}>Organization Mgmt</span>,
       key: 'org',
-      icon: <img src={(selectedKeys.includes('org') || selectedKeys.includes('orglist') || selectedKeys.includes('orgtype')) ? orgActiveIcon : orgDefaultIcon} className={styles.menu_icon} />,
+      icon: (
+        <img
+          src={
+            selectedKeys.includes('org') ||
+              selectedKeys.includes('orglist') ||
+              selectedKeys.includes('orgtype')
+              ? orgActiveIcon
+              : orgDefaultIcon
+          }
+          className={styles.menu_icon}
+        />
+      ),
       children: [
         {
-          label: <span className={styles.subMenuItemLabelStyle}>Organization List</span>,
+          label: 'Organization List',
           key: 'orglist',
         },
         {
-          label: <span className={styles.subMenuItemLabelStyle}>Organization Type</span>,
+          label: 'Organization Type',
           key: 'orgtype',
         },
       ],
@@ -38,70 +60,126 @@ const SiderFooter: React.FC<Props> = ({ collapsed }) => {
     {
       label: <span className={styles.menuItemLabelStyle}>Role Mgmt</span>,
       key: 'role',
-      icon: <img src={selectedKeys.includes('role') ? roleActiveIcon : roleDefaultIcon} className={styles.menu_icon} />,
+      icon: (
+        <img
+          src={selectedKeys.includes('role') ? roleActiveIcon : roleDefaultIcon}
+          className={styles.menu_icon}
+        />
+      ),
     },
     {
       label: <span className={styles.menuItemLabelStyle}>User Mgmt</span>,
       key: 'user',
-      icon: <img src={selectedKeys.includes('user') ? userActiveIcon : userDefaultIcon} className={styles.menu_icon} />,
+      icon: (
+        <img
+          src={selectedKeys.includes('user') ? userActiveIcon : userDefaultIcon}
+          className={styles.menu_icon}
+        />
+      ),
     },
   ]
 
-  const handleMenuClick = (e: any) => {
-    if (e.key === 'orglist') {
-      navigate('/organization/list');
-    }
-    if (e.key === 'orgtype') {
-      navigate('/organization/type');
-    }
-    if (e.key === 'role') {
-      navigate('/role');
-    }
-    if (e.key === 'user') {
-      navigate('/user');
-    };
-  }
+  /* =======================
+     collapsed 自定义渲染
+  ======================== */
 
-  const languageItems: MenuProps['items'] = [
-    { key: '1', label: 'Organization List' },
-    { key: '2', label: 'Organization Type' },
-  ]
+  const renderCollapsedMenu = () => {
+    return menuItems?.map((item: any) => {
+      const hasChildren = item.children?.length > 0
+
+      const stylesFn: PopoverProps['styles'] = (info) => {
+        if (!info.props.arrow) {
+          return {
+            container: {
+              backgroundColor: 'transparent',
+              boxShadow: 'none',
+              padding: 0,
+            },
+          } satisfies PopoverProps['styles'];
+        }
+        return {};
+      };
+      const isActive =
+        selectedKeys.includes(item.key) ||
+        item.children?.some((sub: any) =>
+          selectedKeys.includes(sub.key)
+        )
+      return (
+        <Popover
+          key={item.key}
+          placement="right"
+          trigger="hover"
+          arrow={false}
+          styles={stylesFn}
+          align={{
+            offset: [10, 0],
+          }}
+          content={
+            <div className={styles.subMenuCollapsedStyle}>
+              {hasChildren ? (
+                item.children.map((sub: any) => (
+                  <div
+                    key={sub.key}
+                    className={styles.subMenuItem}
+                    onClick={() => handleMenuClick(sub.key)}
+                  >
+                    {sub.label}
+                  </div>
+                ))
+              ) : (
+                <div
+                  className={styles.subMenuItem}
+                  onClick={() => handleMenuClick(item.key)}
+                >
+                  {item.label}
+                </div>
+              )}
+            </div>
+          }
+        >
+          <div className={`${styles.iconOnlyItem} ${isActive ? styles.activeIconOnlyItem : ''
+            }`}>
+            {item.icon}
+            {isActive && (
+              <div className={styles.activeBar} />
+            )}
+          </div>
+        </Popover>
+      )
+    })
+  }
 
   return (
     <ConfigProvider
       theme={{
         components: {
           Menu: {
-            // itemSelectedBg: '#33C2C8',     // 选中背景色
-            // subMenuItemBg: '#f7f7f7',// 子菜单未选中背景色
-            // itemActiveBg: '#33C2C8',
             activeBarBorderWidth: 0,
           },
         },
       }}
     >
-      <Menu
-        // theme={'dark'} 
-        theme={'light'}
-        mode="inline"
-        items={menuItems}
-        onSelect={({ key }) => setSelectedKeys([key])}
-        expandIcon={({ isOpen }) => <img src={isOpen ? menuIconOpen : menuIconClose} className={styles.expandIcon} />}
-        className={`${styles.menu} ${collapsed ? styles.collapsedMenu : ''}`}
-        inlineIndent={16} // padding-left
-        onClick={handleMenuClick}
-        popupRender={() => (
-          <div
-            className={styles.subMenuCollapsedStyle}
-          >
-            {languageItems?.map((item: any) => (
-              <div key={item.key} className={styles.subMenuItem}>
-                {item.label}
-              </div>
-            ))}
-          </div>
-        )}
-      />
+      {!collapsed ? (
+        <Menu
+          theme="light"
+          mode="inline"
+          items={menuItems}
+          selectedKeys={selectedKeys}
+          onClick={({ key }) => handleMenuClick(key)}
+          expandIcon={({ isOpen }) => (
+            <img
+              src={isOpen ? menuIconOpen : menuIconClose}
+              className={styles.expandIcon}
+            />
+          )}
+          className={styles.menu}
+          inlineIndent={16}
+        />
+      ) : (
+        <div className={styles.collapsedWrapper}>
+          {renderCollapsedMenu()}
+        </div>
+      )}
     </ConfigProvider>
   )
 }
