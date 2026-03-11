6fc56d3765638b0c20182f3bf785d0c3f9a269e9
zhaojian <zhao.jian01@pylontech.com.cn>
2026-03-11 17:32:06 +0800

    feat: 优化目录结构

--- src/pages/organization/components/organization-info/OrganizationView.tsx
+++ src/pages/organization/components/organization-info/OrganizationView.tsx
@@ -134,6 +134,15 @@ const OrganizationView: React.FC<OrganizationViewIProps> = ({
   const handleSubmit = useCallback(
     async (values: OrganizationFormData) => {
       try {
+        if (!values.orgCountryRegion) {
+          form.setFields([
+            {
+              name: 'orgAddress',
+              errors: ['Please select correct country/region to create organization.'],
+            },
+          ]);
+          return { success: false, reason: 'validation_failed' };
+        }
         setSpinning(true);
         setLoading(true);
         const verifyData = await verifyOrganizationByUpdate({ ...values, orgId });
