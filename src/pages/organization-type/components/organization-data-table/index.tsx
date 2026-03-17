fd02c72c9ac4f44c333cfdd7b48c3a99729ad1be
du.mingjie <du.mingjie@pylontech.com.cn>
2026-03-17 09:48:27 +0800

    feat:组织类型-页面顶部搜索框和刷新按钮替换为封装后的组件

--- src/pages/organization-type/components/organization-data-table/index.tsx
+++ src/pages/organization-type/components/organization-data-table/index.tsx
@@ -7,7 +7,7 @@ import { ReloadOutlined, SearchOutlined } from '@ant-design/icons';
 import type { OrganizationTypeDataItem } from '@shared/types/organizationType';
 import { Input, Select, Spin, Table } from 'antd';
 
-import { SearchInput, Segmented } from '@/components';
+import { FormButton, SearchInput, Segmented } from '@/components';
 // 导入API
 import organizationTypeApi from '@/services/modules/organization/organizationTypeApi';
 
@@ -327,19 +327,34 @@ const OrganizationDataTable: React.FC<OrganizationDataTableProps> = ({
           onChange={(value) => setActiveTab(value as string)}
         />
         <div className={styles.searchContainer}>
-          <Input
+          <SearchInput
+            allowClear={true}
             placeholder="Please enter data field"
-            prefix={<SearchOutlined />}
-            style={{ width: 200 }}
             value={searchText}
-            onChange={(e) => {
-              setSearchText(e.target.value);
-            }}
-            onPressEnter={handleSearch}
+            onChange={(e) => setSearchText(e.target.value)}
           />
-          <div className={styles.refreshIcon} onClick={handleRefresh}>
-            <ReloadOutlined />
-          </div>
+          <FormButton
+            className={styles.refreshBtn}
+            icon={
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13.4001 7.0001C13.4001 3.46548 10.5347 0.600098 7.0001 0.600098C3.46548 0.600098 0.600098 3.46548 0.600098 7.0001C0.600098 10.5347 3.46548 13.4001 7.0001 13.4001C8.51148 13.4001 9.90051 12.8762 10.9955 12.0001M10.9955 12.0001L9.80049 11.5001M10.9955 12.0001L10.7706 13.4001M8.00049 7.0001C8.00049 7.55238 7.55277 8.0001 7.00049 8.0001C6.4482 8.0001 6.00049 7.55238 6.00049 7.0001C6.00049 6.44781 6.4482 6.0001 7.00049 6.0001C7.55277 6.0001 8.00049 6.44781 8.00049 7.0001Z"
+                  stroke="#191B1F"
+                  strokeOpacity="0.4"
+                  strokeWidth="1.2"
+                  strokeLinecap="round"
+                />
+              </svg>
+            }
+            onClick={() => handleRefresh()}
+            title="Refresh"
+          />
         </div>
       </div>
 
