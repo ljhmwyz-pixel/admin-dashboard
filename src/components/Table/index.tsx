97d16df2a1747a6883629cf8c86faeba35b080e8
du.mingjie <du.mingjie@pylontech.com.cn>
2026-03-17 17:34:29 +0800

    ``` feat(components): 新增TableSelect组件并优化表格选择功能 style(organization-type): 优化表格编辑和只读模式样式 ```

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -124,7 +124,7 @@ function BaseTable<RecordType extends object = any>({
         rowSelection={finalRowSelection}
         columns={finalColumns}
         dataSource={filteredData}
-        size="small"
+        size="large"
         className={classNames(styles.baseTable, className)}
         pagination={false}
       />
--- src/components/index.ts
+++ src/components/index.ts
@@ -15,6 +15,7 @@ export { default as SiderFooter } from './layouts/SiderFooter';
 export { default as SiderHeader } from './layouts/SiderHeader';
 export { default as SiderMenu } from './layouts/SiderMenu';
 export { default as FormModal } from './Modal';
+export { default as TableSelect } from './TableSelect';
 /**
  * 分段控制器组件
  */
