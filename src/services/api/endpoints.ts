50b45aa6af8f1b348e87ec590a314fed18f566c7
Zhang rui <1694246305@qq.com>
2026-03-19 18:54:17 +0800

    角色调整

--- src/services/api/endpoints.ts
+++ src/services/api/endpoints.ts
@@ -129,7 +129,7 @@ export const ORG_ROLE_ENDPOINTS = {
   DELETE: (roleId: string) => `/api/v1/organization/roles/${roleId}`,
   LOG: (roleId: string) => `/api/v1/organization/roles/${roleId}/changes`,
   PERMISSIONS: `/api/v1/platform/permissions`,
-  PLATFORM: `/api/v1/platform/type`,
+  PLATFORM: `/api/v1/platform/platforms`,
 } as const;
 
 export default API_ENDPOINTS;
